d63c323f1e206d87d903c515148602a117313470
ChoiSuJin03 <106504517+ChoiSuJin03@users.noreply.github.com>
2025-04-07 12:03:58 +0900

    시간 입력까지

--- todo_peep/lib/screens/detaillist/detail_schedule.dart
+++ todo_peep/lib/screens/detaillist/detail_schedule.dart
@@ -4,11 +4,17 @@ import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:todo_peep/controllers/detail_task_controller.dart';
 import 'package:todo_peep/widgets/team/select_date.dart';
+import 'package:todo_peep/widgets/detail/select_time.dart';
 
 class AddScheduleDetaillist extends GetView<DetailTaskController> {
   AddScheduleDetaillist({super.key});
 
   final List<String> items = ['frontend', 'backend', 'design'];
+  final timeController = Get.find<TimeController>();
+
+  final TextEditingController scheduleController = TextEditingController();
+  final TextEditingController descriptionController = TextEditingController();
+  final TextEditingController dateController = TextEditingController();
 
   String? getToday() {
     DateTime now = DateTime.now();
@@ -17,21 +23,9 @@ class AddScheduleDetaillist extends GetView<DetailTaskController> {
     return strToday;
   }
 
-  final TextEditingController scheduleController = TextEditingController();
-  final TextEditingController descriptionController = TextEditingController();
-  final TextEditingController dateController = TextEditingController();
-
-  // void _showTimePicker(BuildContext context) async {
-  //   TimeOfDay? pickedTime = await showTimePicker(
-  //     context: context,
-  //     initialTime: TimeOfDay.now(),
-  //   );
-
-  //   if (pickedTime != null) {
-  //     // 선택한 시간을 컨트롤러에 반영
-  //     controller.startTime.value = pickedTime.format(context);
-  //   }
-  // }
+  String? getTime() {
+    return '없음';
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -278,9 +272,9 @@ class AddScheduleDetaillist extends GetView<DetailTaskController> {
                                       borderRadius: BorderRadius.circular(10),
                                       borderSide: BorderSide.none,
                                     ),
-                                    hintText: controller.start == ''.obs
+                                    hintText: controller.end == ''.obs
                                         ? getToday()
-                                        : controller.start.toString(),
+                                        : controller.end.toString(),
                                     hintStyle: const TextStyle(
                                       fontSize: 14,
                                       color: Color(0xff000000),
@@ -339,35 +333,40 @@ class AddScheduleDetaillist extends GetView<DetailTaskController> {
                               ),
                               SizedBox(
                                 height: 50,
-                                child: TextFormField(
-                                  decoration: InputDecoration(
-                                    contentPadding:
-                                        const EdgeInsets.only(left: 16),
-                                    suffixIcon: const Icon(
-                                      Icons.access_time,
-                                      size: 20,
-                                      color: Color(0xffcfcfcf),
-                                    ),
-                                    filled: true,
-                                    fillColor: const Color(0xffF4F4F4)
-                                        .withOpacity(0.5),
-                                    border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(10),
-                                      borderSide: BorderSide.none,
-                                    ),
-                                    hintText: controller.startTime.value.isEmpty
-                                        ? "00:00"
-                                        : controller.startTime.value,
-                                    hintStyle: const TextStyle(
-                                      fontSize: 14,
-                                      color: Color(0xff000000),
-                                    ),
-                                  ),
-                                  readOnly: true,
-                                  onTap: () {
-                                    controller.showTimePicker(context);
-                                  },
-                                ),
+                                child: Obx(() => TextFormField(
+                                      controller: timeController
+                                          .startTimeTextController,
+                                      decoration: InputDecoration(
+                                        contentPadding:
+                                            const EdgeInsets.only(left: 16),
+                                        suffixIcon: const Icon(
+                                          Icons.access_time,
+                                          size: 20,
+                                          color: Color(0xffcfcfcf),
+                                        ),
+                                        filled: true,
+                                        fillColor: const Color(0xffF4F4F4)
+                                            .withOpacity(0.5),
+                                        border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                          borderSide: BorderSide.none,
+                                        ),
+                                        hintText: timeController
+                                                .startTime.value.isEmpty
+                                            ? getTime()
+                                            : timeController.startTime.value,
+                                        hintStyle: const TextStyle(
+                                          fontSize: 14,
+                                          color: Color(0xff000000),
+                                        ),
+                                      ),
+                                      readOnly: true,
+                                      onTap: () {
+                                        timeController.showTimePicker(context,
+                                            isStart: true);
+                                      },
+                                    )),
                               ),
                             ],
                           ),
@@ -455,7 +454,7 @@ class AddScheduleDetaillist extends GetView<DetailTaskController> {
                             ],
                           ),
                         ),
-                        const SizedBox(width: 10), // 종료 날짜와 종료 시간 간격
+                        const SizedBox(width: 10),
                         Expanded(
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
@@ -470,38 +469,81 @@ class AddScheduleDetaillist extends GetView<DetailTaskController> {
                               ),
                               SizedBox(
                                 height: 50,
-                                child: TextFormField(
-                                  decoration: InputDecoration(
-                                    contentPadding:
-                                        const EdgeInsets.only(left: 16),
-                                    suffixIcon: const Icon(
-                                      Icons.access_time,
-                                      size: 20,
-                                      color: Color(0xffcfcfcf),
-                                    ),
-                                    filled: true,
-                                    fillColor: const Color(0xffF4F4F4)
-                                        .withOpacity(0.5),
-                                    border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(10),
-                                      borderSide: BorderSide.none,
-                                    ),
-                                    hintText: controller.endTime.value.isEmpty
-                                        ? "00:00"
-                                        : controller.endTime.value,
-                                    hintStyle: const TextStyle(
-                                      fontSize: 14,
-                                      color: Color(0xff000000),
-                                    ),
-                                  ),
-                                  readOnly: true,
-                                  onTap: () {
-                                    controller
-                                        .showEndTimePicker(context); // 종료 시간 선택
-                                  },
-                                ),
+                                child: Obx(() => TextFormField(
+                                      controller:
+                                          timeController.endTimeTextController,
+                                      decoration: InputDecoration(
+                                        contentPadding:
+                                            const EdgeInsets.only(left: 16),
+                                        suffixIcon: const Icon(
+                                          Icons.access_time,
+                                          size: 20,
+                                          color: Color(0xffcfcfcf),
+                                        ),
+                                        filled: true,
+                                        fillColor: const Color(0xffF4F4F4)
+                                            .withOpacity(0.5),
+                                        border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                          borderSide: BorderSide.none,
+                                        ),
+                                        hintText:
+                                            timeController.endTime.value.isEmpty
+                                                ? getTime()
+                                                : timeController.endTime.value,
+                                        hintStyle: const TextStyle(
+                                          fontSize: 14,
+                                          color: Color(0xff000000),
+                                        ),
+                                      ),
+                                      readOnly: true,
+                                      onTap: () {
+                                        timeController.showTimePicker(context,
+                                            isStart: false);
+                                      },
+                                    )),
                               ),
                             ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 15),
+                    Row(
+                      children: [
+                        GestureDetector(
+                          onTap: controller.onCategoryPinToggle,
+                          child: Container(
+                            width: 20,
+                            height: 20,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.rectangle, // 체크박스 모양 (사각형)
+                              borderRadius: BorderRadius.circular(5), // 모서리 둥글기
+                              border: Border.all(
+                                color: controller.categoryPin.value
+                                    ? const Color(0xff424656)
+                                    : const Color(
+                                        0xffD1D1D6), // 체크 여부에 따른 테두리 색상
+                                width: 1,
+                              ),
+                              color: controller.categoryPin.value
+                                  ? const Color(0xff424656)
+                                  : Colors.white, // 체크 여부에 따른 배경색
+                            ),
+                            child: controller.categoryPin.value
+                                ? const Icon(Icons.check,
+                                    size: 18, color: Colors.white)
+                                : null,
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                        const Text(
+                          "루틴으로 설정하기",
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                            color: Color(0xff525252),
                           ),
                         ),
                       ],
--- todo_peep/lib/widgets/detail/select_time.dart
+++ todo_peep/lib/widgets/detail/select_time.dart
@@ -1,16 +1,32 @@
 import 'package:get/get.dart';
-import 'package:intl/intl.dart';
-import 'package:flutter/cupertino.dart'; // CupertinoTimerPicker 추가
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:todo_peep/widgets/detail/select_time.dart';
 
 class TimeController extends GetxController {
-  var startTime = ''.obs;
-  var endTime = ''.obs;
+  var startTime = '없음'.obs;
+  var endTime = '없음'.obs;
+
+  final startTimeTextController = TextEditingController();
+  final endTimeTextController = TextEditingController();
+
+  void showTimePicker(BuildContext context, {required bool isStart}) {
+    showCustomTimePicker(context, isStart: isStart);
+  }
+
+  // @override
+  // void onInit() {
+  //   super.onInit();
+
+  //   startTime.value = '없음';
+  //   endTime.value = '없음';
+
+  //   startTimeTextController.text = '없음';
+  //   endTimeTextController.text = '없음';
+  // }
 }
 
 void showCustomTimePicker(BuildContext context, {required bool isStart}) {
-  final timeController = Get.find<TimeController>(); // TimeController 가져오기
+  final timeController = Get.find<TimeController>();
 
   int selectedHour = TimeOfDay.now().hour;
   int selectedMinute = TimeOfDay.now().minute;
@@ -24,90 +40,178 @@ void showCustomTimePicker(BuildContext context, {required bool isStart}) {
       return StatefulBuilder(
         builder: (context, setState) {
           return Container(
-            height: 300,
+            width: MediaQuery.of(context).size.width - 20,
+            height: 370,
             padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
+                Container(
+                  width: 40,
+                  height: 4,
+                  margin: const EdgeInsets.only(bottom: 16),
+                  decoration: BoxDecoration(
+                    color: Color(0xffcfcfcf), // <- 너가 쓰던 아이콘 색이랑 똑같이!
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
                 Expanded(
-                  child: Row(
+                  child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      // 시간 선택 피커
-                      Expanded(
-                        child: CupertinoPicker(
-                          itemExtent: 40,
-                          scrollController: FixedExtentScrollController(
-                              initialItem: selectedHour),
-                          onSelectedItemChanged: (int index) {
-                            setState(() {
-                              selectedHour = index;
-                            });
-                          },
-                          children: List.generate(24, (index) {
-                            return Center(
-                              child: Text(
-                                index.toString().padLeft(2, '0'),
-                                style: const TextStyle(fontSize: 20),
+                      // ✅ 회색 배경 + 버튼 너비 동일한 피커 영역
+                      Container(
+                        width: 332,
+                        height: 160,
+                        decoration: BoxDecoration(
+                          color: const Color(0xffF4F4F4).withOpacity(0.5),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Row(
+                          children: [
+                            // 시간 선택 피커
+                            Expanded(
+                              child: CupertinoPicker(
+                                itemExtent: 40,
+                                scrollController: FixedExtentScrollController(
+                                    initialItem: selectedHour),
+                                onSelectedItemChanged: (int index) {
+                                  setState(() {
+                                    selectedHour = index;
+                                  });
+                                },
+                                children: List.generate(24, (index) {
+                                  return Center(
+                                    child: Text(
+                                      index.toString().padLeft(2, '0'),
+                                      style: const TextStyle(fontSize: 20),
+                                    ),
+                                  );
+                                }),
                               ),
-                            );
-                          }),
+                            ),
+                            // 분 선택 피커
+                            Expanded(
+                              child: CupertinoPicker(
+                                itemExtent: 40,
+                                scrollController: FixedExtentScrollController(
+                                    initialItem: selectedMinute),
+                                onSelectedItemChanged: (int index) {
+                                  setState(() {
+                                    selectedMinute = index;
+                                  });
+                                },
+                                children: List.generate(60, (index) {
+                                  return Center(
+                                    child: Text(
+                                      index.toString().padLeft(2, '0'),
+                                      style: const TextStyle(fontSize: 20),
+                                    ),
+                                  );
+                                }),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                      const Text(":", style: TextStyle(fontSize: 20)),
-                      // 분 선택 피커
-                      Expanded(
-                        child: CupertinoPicker(
-                          itemExtent: 40,
-                          scrollController: FixedExtentScrollController(
-                              initialItem: selectedMinute),
-                          onSelectedItemChanged: (int index) {
-                            setState(() {
-                              selectedMinute = index;
-                            });
+
+                      const SizedBox(height: 40), // 버튼과 간격 주기
+
+                      // 완료 버튼
+                      SizedBox(
+                        width: 332,
+                        height: 50,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xff424656),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          onPressed: () {
+                            final formattedTime =
+                                '${selectedHour.toString().padLeft(2, '0')}:${selectedMinute.toString().padLeft(2, '0')}';
+
+                            if (isStart) {
+                              // 종료 시간이 이미 설정되어 있다면 비교!
+                              final end = timeController.endTime.value;
+                              if (end != '없음') {
+                                final endHour =
+                                    int.parse(end.split(':')[0].trim());
+                                final endMinute =
+                                    int.parse(end.split(':')[1].trim());
+
+                                final selectedTotalMinutes =
+                                    selectedHour * 60 + selectedMinute;
+                                final endTotalMinutes =
+                                    endHour * 60 + endMinute;
+
+                                if (selectedTotalMinutes >= endTotalMinutes) {
+                                  Get.snackbar(
+                                    "시간 설정 오류",
+                                    "시작 시간은 종료 시간보다 빨라야 합니다.",
+                                    backgroundColor: const Color.fromARGB(
+                                        255, 228, 159, 159),
+                                    colorText: Colors.white,
+                                    duration: Duration(seconds: 2),
+                                  );
+                                  return; // 저장 안 함
+                                }
+                              }
+
+                              // 정상 저장
+                              timeController.startTime.value = formattedTime;
+                              timeController.startTimeTextController.text =
+                                  formattedTime;
+                              print(
+                                  "✅ 시작 시간 설정됨: ${timeController.startTime.value}");
+                              Navigator.pop(context);
+                            } else {
+                              // 시작 시간과 비교
+                              final start = timeController.startTime.value;
+                              final startHour =
+                                  int.parse(start.split(':')[0].trim());
+                              final startMinute =
+                                  int.parse(start.split(':')[1].trim());
+
+                              final selectedTotalMinutes =
+                                  selectedHour * 60 + selectedMinute;
+                              final startTotalMinutes =
+                                  startHour * 60 + startMinute;
+
+                              if (selectedTotalMinutes <= startTotalMinutes) {
+                                Get.snackbar(
+                                  "시간 설정 오류",
+                                  "종료 시간은 시작 시간보다 빠를 수 없습니다.",
+                                  backgroundColor:
+                                      const Color.fromARGB(255, 228, 159, 159),
+                                  colorText: Colors.white,
+                                  duration: Duration(seconds: 2),
+                                );
+                                return; // 저장 안 함
+                              }
+
+                              timeController.endTime.value = formattedTime;
+                              timeController.endTimeTextController.text =
+                                  formattedTime;
+                              print(
+                                  "✅ 종료 시간 설정됨: ${timeController.endTime.value}");
+                              Navigator.pop(context);
+                            }
                           },
-                          children: List.generate(60, (index) {
-                            return Center(
-                              child: Text(
-                                index.toString().padLeft(2, '0'),
-                                style: const TextStyle(fontSize: 20),
+                          child: Center(
+                            child: Text(
+                              "완료",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
                               ),
-                            );
-                          }),
+                            ),
+                          ),
                         ),
                       ),
                     ],
-                  ),
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xff424656),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    onPressed: () {
-                      final formattedTime =
-                          '${selectedHour.toString().padLeft(2, '0')}:${selectedMinute.toString().padLeft(2, '0')}';
-
-                      if (isStart) {
-                        timeController.startTime.value =
-                            formattedTime; // 수정된 부분
-                      } else {
-                        timeController.endTime.value = formattedTime; // 수정된 부분
-                      }
-
-                      Navigator.pop(context);
-                    },
-                    child: const Text(
-                      "완료",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                      ),
-                    ),
                   ),
                 ),
               ],
